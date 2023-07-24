7f7a0fa544f50901e2cc6676d7eef27856b402c7
Vilhelm Bergsøe <vilhelmbergsoe@gmail.com>
2023-07-24 20:17:05 +0200

    move legality and other work on api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+const WHITE_PAWN: usize = 0;
+const WHITE_KNIGHT: usize = 1;
+const WHITE_BISHOP: usize = 2;
+const WHITE_ROOK: usize = 3;
+const WHITE_QUEEN: usize = 4;
+const WHITE_KING: usize = 5;
+const BLACK_PAWN: usize = 6;
+const BLACK_KNIGHT: usize = 7;
+const BLACK_BISHOP: usize = 8;
+const BLACK_ROOK: usize = 9;
+const BLACK_QUEEN: usize = 10;
+const BLACK_KING: usize = 11;
+
 #[derive(Debug)]
 enum Piece {
     Pawn,
@@ -14,10 +27,15 @@ enum Color {
     Black,
 }
 
+#[derive(Debug)]
 struct Square(u8);
 
+// impl std::fmt::Display for Square {
+
+// }
+
 impl Square {
-    fn new(file: u8, rank: u8) -> Square {
+    fn from(file: u8, rank: u8) -> Square {
         Square(rank * 8 + file)
     }
 
@@ -35,7 +53,7 @@ impl Square {
             return None;
         }
 
-        Some(Square::new(file - b'a', rank - b'1'))
+        Some(Square::from(file - b'a', rank - b'1'))
     }
 
     fn to_algebraic(&self) -> String {
@@ -58,8 +76,8 @@ struct Board {
     bitboard: [u64; 12],
 }
 
-impl Board {
-    fn new() -> Board {
+impl std::default::Default for Board {
+    fn default() -> Self {
         Board {
             // Default starting position
             bitboard: [
@@ -78,7 +96,13 @@ impl Board {
             ],
         }
     }
-    fn get_piece_at(&self, square: Square) -> Option<(Piece, Color)> {
+}
+
+impl Board {
+    fn new() -> Board {
+        Board { bitboard: [0; 12] }
+    }
+    fn get_piece_at(&self, square: &Square) -> Option<(Piece, Color)> {
         let square = square.0;
 
         if square > 63 {
@@ -87,36 +111,63 @@ impl Board {
 
         for (index, &bitboard) in self.bitboard.iter().enumerate() {
             if bitboard & (1u64 << square) != 0 {
-                let piece = match index {
-                    0 | 6 => Piece::Pawn,
-                    1 | 7 => Piece::Knight,
-                    2 | 8 => Piece::Bishop,
-                    3 | 9 => Piece::Rook,
-                    4 | 10 => Piece::Queen,
-                    5 | 11 => Piece::King,
+                match index {
+                    WHITE_PAWN => return Some((Piece::Pawn, Color::White)),
+                    BLACK_PAWN => return Some((Piece::Pawn, Color::Black)),
+                    WHITE_KNIGHT => return Some((Piece::Knight, Color::White)),
+                    BLACK_KNIGHT => return Some((Piece::Knight, Color::Black)),
+                    WHITE_BISHOP => return Some((Piece::Bishop, Color::White)),
+                    BLACK_BISHOP => return Some((Piece::Bishop, Color::Black)),
+                    WHITE_ROOK => return Some((Piece::Rook, Color::White)),
+                    BLACK_ROOK => return Some((Piece::Rook, Color::Black)),
+                    WHITE_QUEEN => return Some((Piece::Queen, Color::White)),
+                    BLACK_QUEEN => return Some((Piece::Queen, Color::Black)),
+                    WHITE_KING => return Some((Piece::King, Color::White)),
+                    BLACK_KING => return Some((Piece::King, Color::Black)),
                     _ => unreachable!(),
                 };
-
-                let color = if index < 6 {
-                    Color::White
-                } else {
-                    Color::Black
-                };
-
-                return Some((piece, color));
             }
         }
 
         None
     }
     fn set_piece_at(&mut self, square: Square, piece: (Piece, Color)) {
-        let square = square.0;
-
-        if square > 63 {
+        if square.0 > 63 {
             return;
         }
 
-        let index = match (piece.0, piece.1) {
+        self.remove_piece_at(&square);
+
+        let idx = self.get_piece_idx(piece);
+
+        self.bitboard[idx] |= 1u64 << square.0;
+    }
+    fn remove_piece_at(&mut self, square: &Square) {
+        let prev_piece = match self.get_piece_at(square) {
+            Some(v) => v,
+            None => return,
+        };
+
+        let idx = self.get_piece_idx(prev_piece);
+
+        self.bitboard[idx] &= !(1u64 << square.0);
+    }
+    fn move_piece(&mut self, from: Square, to: Square) {
+        let piece = self.get_piece_at(&from).unwrap();
+
+        if from.0 > 63 || to.0 > 63 {
+            return;
+        }
+
+        self.remove_piece_at(&to);
+
+        let idx = self.get_piece_idx(piece);
+
+        self.bitboard[idx] &= !(1u64 << from.0);
+        self.bitboard[idx] |= 1u64 << to.0;
+    }
+    fn get_piece_idx(&self, piece: (Piece, Color)) -> usize {
+        match piece {
             (Piece::Pawn, Color::White) => 0,
             (Piece::Knight, Color::White) => 1,
             (Piece::Bishop, Color::White) => 2,
@@ -129,21 +180,76 @@ impl Board {
             (Piece::Rook, Color::Black) => 9,
             (Piece::Queen, Color::Black) => 10,
             (Piece::King, Color::Black) => 11,
+        }
+    }
+    fn get_legal_moves(&self, square: Square) -> Option<Vec<Square>> {
+        let (piece, color) = match self.get_piece_at(&square) {
+            Some(piece_color) => piece_color,
+            None => return None, // No piece at the given square
         };
 
-        self.bitboard[index] |= 1u64 << square;
+        match piece {
+            Piece::Pawn => self.get_pawn_moves(square, color),
+            _ => todo!(),
+        }
     }
-    fn move_piece(&mut self, from: Square, to: Square) {
-        let from = from.0;
-        let to = to.0;
 
-        if from > 63 || to > 63 {
-            return;
+    fn get_pawn_moves(&self, square: Square, color: Color) -> Option<Vec<Square>> {
+        let direction = match color {
+            Color::White => 1,
+            Color::Black => -1,
+        };
+
+        let mut moves: Vec<Square> = Vec::new();
+
+        // Normal pawn move (one square forward)
+        let forward_one = square.0 as i8 + direction * 8;
+        if forward_one >= 0 && forward_one < 64 {
+            let target_square = Square(forward_one as u8);
+            if self.get_piece_at(&target_square).is_none() {
+                moves.push(target_square);
+            }
         }
 
-        for bitboard in self.bitboard.iter_mut() {
-            *bitboard &= !(1u64 << from);
-            *bitboard |= 1u64 << to;
+        // Double pawn move (two squares forward from starting position)
+        let starting_rank = match color {
+            Color::White => 0xFF00,
+            Color::Black => 0xFF000000000000,
+        };
+        if (2_u64.pow(square.0.into())) & starting_rank != 0 {
+            let forward_two = square.0 as i8 + (2 * direction * 8);
+            if forward_two >= 0 && forward_two < 64 {
+                let target_square = Square(forward_two as u8);
+                if self.get_piece_at(&target_square).is_none() {
+                    moves.push(target_square);
+                }
+            }
+        }
+
+        // Capture moves
+        // let capture_files = [-1, 1];
+        // for &file_offset in capture_files.iter() {
+        //     let target_file = (square.0 % 8) as i8 + file_offset;
+        //     let target_rank = square.0 as i8 + direction;
+
+        //     if target_file >= 0 && target_file < 8 && target_rank >= 0 && target_rank < 8 {
+        //         let target_square = Square((target_rank * 8 + target_file) as u8);
+        //         if let Some((captured_piece, captured_color)) = self.get_piece_at(target_square) {
+        //             if captured_color != color {
+        //                 moves.push(target_square);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // En passant (to be implemented)
+
+        // Promotion (to be implemented)
+
+        if moves.is_empty() {
+            None
+        } else {
+            Some(moves)
         }
     }
 }
@@ -185,27 +291,98 @@ impl std::fmt::Display for Board {
 }
 
 fn main() {
-    let mut board = Board::new();
+    let mut board = Board::default();
 
     // pretty print the binary representation of the first bitboard
     // println!("{:064b}", board.bitboard[7]);
 
     print!("{}", board);
 
-    let piece = board.get_piece_at(Square::new(0, 0)).unwrap();
+    board.set_piece_at(
+        Square::from_algebraic("a1").unwrap(),
+        (Piece::Pawn, Color::Black),
+    );
 
-    println!("piece at (0, 0) is a {:?} {:?}", piece.1, piece.0);
-
-    board.set_piece_at(Square::new(0, 0), (Piece::Pawn, Color::White));
+    print!("{}", board);
 
     println!(
         "{:?}",
-        board.get_piece_at(Square::from_algebraic("b8").unwrap())
+        board.get_legal_moves(Square::from_algebraic("a1").unwrap())
     );
 
-    println!("{:?}", Square::from_algebraic("b8").unwrap().to_algebraic());
+    board.move_piece(Square(0), Square(8));
 
-    board.move_piece(Square::from_algebraic("a2").unwrap(), Square::from_algebraic("a4").unwrap());
+    board.move_piece(
+        Square::from_algebraic("f2").unwrap(),
+        Square::from_algebraic("f3").unwrap(),
+    );
 
     print!("{}", board);
+
+    println!(
+        "legal moves for b2: {:?}",
+        board
+            .get_legal_moves(Square::from_algebraic("b2").unwrap())
+            .unwrap()
+            .iter()
+            .map(|x| x.to_algebraic())
+            .collect::<Vec<String>>()
+    );
+
+    println!(
+        "legal moves for b7: {:?}",
+        board
+            .get_legal_moves(Square::from_algebraic("b7").unwrap())
+            .unwrap()
+            .iter()
+            .map(|x| x.to_algebraic())
+            .collect::<Vec<String>>()
+    );
+
+    println!(
+        "legal moves for f3: {:?}",
+        board
+            .get_legal_moves(Square::from_algebraic("f3").unwrap())
+            .unwrap()
+            .iter()
+            .map(|x| x.to_algebraic())
+            .collect::<Vec<String>>()
+    );
+    // board.move_piece(
+    //     Square::from_algebraic("c2").unwrap(),
+    //     Square::from_algebraic("c3").unwrap(),
+    // );
+    // print!("{}", board);
+    // println!(
+    //     "{:?}",
+    //     board.get_legal_moves(Square::from_algebraic("c3").unwrap())
+    // );
+
+    // let piece = board.get_piece_at(&Square::from(0, 0)).unwrap();
+
+    // println!("piece at (0, 0) is a {:?} {:?}", piece.1, piece.0);
+
+    // board.set_piece_at(Square::from(0, 0), (Piece::Pawn, Color::White));
+
+    // println!(
+    //     "piece at b8 is {:?}",
+    //     board.get_piece_at(&Square::from_algebraic("b8").unwrap())
+    // );
+
+    // println!("{:?}", Square::from_algebraic("b8").unwrap().to_algebraic());
+
+    // board.move_piece(
+    //     Square::from_algebraic("a2").unwrap(),
+    //     Square::from_algebraic("a4").unwrap(),
+    // );
+
+    // print!("{}", board);
+
+    // board.set_piece_at(Square::from_algebraic("a1").unwrap(), (Piece::Pawn, Color::Black));
+
+    // board.move_piece(Square::from_algebraic("a7").unwrap(), Square::from_algebraic("a5").unwrap());
+
+    // print!("{}", board);
+
+    // println!("legal moves for g8: {:?}", board.get_legal_moves(Square::from_algebraic("a1").unwrap()));
 }
